547968f62818c97f9c5f767af9e8f3e1d47fd475
Joshua W <xeon826@gmail.com>
2024-03-25 00:00:57 -0500

    fixed casing and improved targeting system

--- Lazy.lua
+++ Lazy.lua
@@ -10,7 +10,7 @@ _addon.author = 'Brax'
 _addon.version = '0.5'
 _addon.commands = {'lazy'}
 
-Start_Engine = true
+start_engine = true
 isCasting = false
 isBusy = 0
 buffactive = {}
@@ -27,6 +27,17 @@ defaults.autotarget = false
 defaults.target = ""
 
 settings = config.load(defaults)
+
+local function split_string_by_comma(inputStr)
+    local result = {}
+    for match in (inputStr..','):gmatch("(.-)"..',') do
+        table.insert(result, match)
+    end
+    return result
+end
+
+
+settings.targets = split_string_by_comma(settings.targets)
 
 windower.register_event('incoming chunk', function(id, data)
     if id == 0x028 then
@@ -56,11 +67,11 @@ windower.register_event('addon command', function (...)
 		print("Help Info")
 	elseif args[1] == "start" then
 		windower.add_to_chat(2,"....Starting Lazy Helper....")
-		Start_Engine = true
-		Engine()
+		start_engine = true
+		engine()
 	elseif args[1] == "stop" then
 		windower.add_to_chat(2,"....Stopping Lazy Helper....")
-		Start_Engine = false
+		start_engine = false
 	elseif args[1] == "reload" then
 		windower.add_to_chat(2,"....Reloading Config....")
 		config.reload(settings)
@@ -74,7 +85,7 @@ windower.register_event('addon command', function (...)
 		windower.add_to_chat(11,"Use Spell "..tostring(settings.spell_active))
 		windower.add_to_chat(11,"Weaponskill: "..settings.weaponskill)
 		windower.add_to_chat(11,"Use Weaponskill: "..tostring(settings.weaponskill_active))
-		windower.add_to_chat(11,"Target:"..settings.target)
+		-- windower.add_to_chat(11,"Target:"..settings.target)
 	elseif args[1] == "autotarget" then
 		if args[2] == "on" then
 			settings.autotarget = true
@@ -83,33 +94,34 @@ windower.register_event('addon command', function (...)
 			settings.autotarget = false
 			windower.add_to_chat(3,"Autotarget: False")
 		end
-	elseif args[1] == "target" then
-		settings.target = args[2]
-	end
+    end
+	-- elseif args[1] == "target" then
+	-- 	settings.target = args[2]
+	-- end
 end)
 
-function HeadingTo(X,Y)
+function heading_to(X,Y)
 	local X = X - windower.ffxi.get_mob_by_id(windower.ffxi.get_player().id).x
 	local Y = Y - windower.ffxi.get_mob_by_id(windower.ffxi.get_player().id).y
 	local H = math.atan2(X,Y)
 	return H - 1.5708
 end
 
-function TurnToTarget()
+function turn_to_target()
 	local destX = windower.ffxi.get_mob_by_target('t').x
 	local destY = windower.ffxi.get_mob_by_target('t').y
-	local direction = math.abs(PlayerH - math.deg(HeadingTo(destX,destY)))
+	local direction = math.abs(PlayerH - math.deg(heading_to(destX,destY)))
 	if direction > 10 then
-		windower.ffxi.turn(HeadingTo(destX,destY))
+		windower.ffxi.turn(heading_to(destX,destY))
 	end
 end
 
-function Find_Nearest_Target(target)
+function find_nearest_target(target)
 	local id_targ = -1
 	local dist_targ = -1
 	local marray = windower.ffxi.get_mob_array()
 	for key,mob in pairs(marray) do
-		if string.lower(mob["name"]) == string.lower(target) and mob["valid_target"] and mob["hpp"] == 100 then
+		if contains(settings.targets, string.lower(mob['name'])) and mob["valid_target"] and mob["hpp"] == 100 then
 			if dist_targ == -1 then
 				id_targ = key
 				dist_targ = math.sqrt(mob["distance"])
@@ -122,10 +134,10 @@ function Find_Nearest_Target(target)
 	return(id_targ)
 end
 
-function Check_Distance()
+function check_distance()
 	local distance = windower.ffxi.get_mob_by_target('t').distance:sqrt()
 	if distance > 3 then
-		TurnToTarget()
+		turn_to_target()
 		windower.ffxi.run()
 	else
 		windower.ffxi.run(false)
@@ -135,43 +147,44 @@ end
 function test()
 end
 
-function Engine()
+function engine()
 	Buffs = windower.ffxi.get_player()["buffs"]
     table.reassign(buffactive,convert_buff_list(Buffs))
 
 	if isBusy < 1 then
-		pcall(Combat)
+		pcall(combat)
 	else
 		isBusy = isBusy -1
 	end
-	if Start_Engine then
-		coroutine.schedule(Engine,1)
+	if start_engine then
+		coroutine.schedule(engine,1)
 	end
 end
 
-function Combat()
+function combat()
 	-- is Engaged / combat
 	if windower.ffxi.get_player().status == 1 then
-		TurnToTarget()
-		Check_Distance()
+		turn_to_target()
+		check_distance()
 		if windower.ffxi.get_player().vitals.tp >1000 and settings.weaponskill_active == true and windower.ffxi.get_mob_by_target('t').distance:sqrt() < 3.0 then
 			windower.send_command(settings.weaponskill)
 			isBusy = Action_Delay
-		elseif Can_Cast_Spell(settings.spell) and settings.spell_active == true then
-			Cast_Spell(settings.spell)
+		elseif can_cast_spell(settings.spell) and settings.spell_active == true then
+			cast_spell(settings.spell)
 		end
 	elseif settings.autotarget == true then
-		if Find_Nearest_Target(settings.target) > 0 then
-			windower.ffxi.follow(Find_Nearest_Target(settings.target))
-			if math.sqrt(windower.ffxi.get_mob_by_index(Find_Nearest_Target(settings.target)).distance) < 3 then
-				windower.send_command("input /targetbnpc")
+		if find_nearest_target(settings.targets) > 0 then
+			windower.ffxi.follow(find_nearest_target(settings.targets))
+			if math.sqrt(windower.ffxi.get_mob_by_index(find_nearest_target(settings.targets)).distance) < 3 then
+				-- windower.send_command("input /targetbnpc")
+				target_nearest(settings.targets)
 				windower.send_command("input /attack on")
 			end
 		end
 	end
 end
 
-function Can_Cast_Spell(spell)
+function can_cast_spell(spell)
 	local result = false
 	local myspell = res.spells:with('name',spell)
 	Recasts = windower.ffxi.get_spell_recasts()
@@ -181,7 +194,7 @@ function Can_Cast_Spell(spell)
 	return result
 end
 
-function Can_Cast_Ability(ability)
+function can_cast_ability(ability)
 	local result = false
 	local myability = res.job_abilities:with('name',ability)
 	Recasts = windower.ffxi.get_ability_recasts()
@@ -192,7 +205,7 @@ function Can_Cast_Ability(ability)
 	return result
 end
 
-function Cast_Spell(spell)
+function cast_spell(spell)
 	Recasts = windower.ffxi.get_spell_recasts()
 	local myspell = res.spells:with('name',spell)
 	if Recasts[myspell.id] == 0 and not isCasting then
@@ -201,7 +214,7 @@ function Cast_Spell(spell)
 	end
 end
 
-function Cast_Ability(ability)
+function cast_ability(ability)
 	Recasts = windower.ffxi.get_ability_recasts()
 	local myability = res.job_abilities:with('name',ability)
 	if Recasts[myability.recast_id] == 0 and not isCasting then
@@ -230,4 +243,44 @@ function convert_buff_list(bufflist)
         end
     end
     return buffarr
+end
+
+function target_nearest(target_names)
+    local mobs = windower.ffxi.get_mob_array()
+    local closest
+    for _, mob in pairs(mobs) do
+		if contains(settings.targets, string.lower(mob['name'])) and mob["valid_target"] and mob["hpp"] == 100 then
+            if not closest or mob.distance < closest.distance then
+                closest = mob
+            end
+        end
+    end
+
+    if not closest then
+		windower.add_to_chat(2,"....Cannot find valid target....")
+        return
+    end
+
+    local player = windower.ffxi.get_player()
+
+    packets.inject(packets.new('incoming', 0x058, {
+        ['Player'] = player.id,
+        ['Target'] = closest.id,
+        ['Player Index'] = player.index,
+    }))
+
+    if player.status == 1 then
+        windower.send_command('wait 0.5; input /attack <t>')
+    end
+end
+
+
+
+function contains(table, val)
+    for _, value in ipairs(table) do
+        if value == val then
+            return true
+        end
+    end
+    return false
 end
